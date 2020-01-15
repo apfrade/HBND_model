c277106db4adc6dd9f2cab9d280ef798768bbe59
apfrade <andr.frade@gmail.com>
2020-01-15 14:23:23 +0000

    done

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ import setuptools
 with open("README.md", "r") as readme_file:
     long_description = readme_file.read()
 
-requirements = ["pandas >= 0.25", "joblib >= 0", "numpy >= 1"]
+requirements = ["pandas >= 0.2", "joblib >= 0.1", "numpy >= 1"]
 
 setuptools.setup(
     name='HBNDmodel',
-    version="0.0.5",
+    version="0.0.6",
     author='Andre Frade',
     author_email="andre.frade@hertford.ox.ac.uk",
     description='HBND predictive model package',
