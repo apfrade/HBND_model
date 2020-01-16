5a4445c971a1f58808aa6d8a9763c682e755cace
apfrade <andr.frade@gmail.com>
2020-01-16 11:32:53 +0000

    added manifest

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ requirements = ["pandas >= 0.2", "joblib >= 0.1", "numpy >= 1"]
 
 setuptools.setup(
     name='HBNDmodel',
-    version="0.0.9",
+    version="0.0.10",
     author='Andre Frade',
     author_email="andre.frade@hertford.ox.ac.uk",
     description='HBND predictive model package',
@@ -17,10 +17,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     install_requires= requirements,
     python_requires='>=3',
-    package_data={
-    'HBNDmodel': ["data/descriptor_names", "data/hbnd_model.sav", "data/scaler_model.bin" ],
-    },
-
+    include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
